9bb0ac5a97c84fa1e2b3751d32a1181855985b21
Andrei <andrey5015n@gmail.com>
2025-09-02 21:01:42 +0300

    Added some missing files

---
+++ main.py
@@ -0,0 +1,12 @@
+from Scraper import Scraper, Filters
+from Database import CONFIG_PATH
+
+query = 'Laptop Lenovo LOQ "Ryzen 5" "3050"'
+config_path = "config.json"
+csv_file = "produse.csv"
+
+scrap = Scraper(CONFIG_PATH)
+
+filter = Filters()
+
+scrap.Run('Telefon samsung a55', filter, 0) 
